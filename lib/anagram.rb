5552e9bca399cbd291c49b113708b43c117facce
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-04 04:27:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,3 +5,8 @@ class Anagram
   attr_accessor :word
   
   def match(array_of_words)
+array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end
+  
